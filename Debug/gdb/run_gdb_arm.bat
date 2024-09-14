927bad7c489f91082bcdfeb5dcdf8aecc1f63e54
Amine Chalandi <amine.chalandi@gmail.com>
2024-09-14 14:07:57 +0200

    update debug env

--- Debug/gdb/run_gdb_arm.bat
+++ Debug/gdb/run_gdb_arm.bat
@@ -2,6 +2,6 @@
 
 start /B ../../Tools/openocd/openocd.exe -f ../../Tools/openocd/rp2350_arm.cfg
 
-arm-none-eabi-gdb -x gdb_arm.cfg ../../Output/Baremetal_Pico2_dual_core_nosdk.elf
+arm-none-eabi-gdb -x gdb_arm.cfg ../../Output/arm_baremetal_pico2_dual_core_nosdk.elf
 
 trap "taskkill /F /IM riscv32-unknown-elf-gdb.exe" EXIT
